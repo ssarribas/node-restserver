4f5a28f5875d2adca856cb3de1c5e59b8d7bfeed
ssarribas <ssarribas@gmail.com>
2020-08-19 18:38:37 +0200

    Añadida variable de entorno para la url de MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:flOE1SYYkp6UzfeG@cluster0.b4pjh.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
